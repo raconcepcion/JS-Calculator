d98db642a7001dd7ca73bb031de824236444b746
Rose Anne Concepcion <roseanne.concepcion@gmail.com>
2014-06-23 19:01:58 +0800

    fix calc

--- calc.js
+++ calc.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-=======
-//sample comment
->>>>>>> 92369294266b290c67fb83fcb707715cab4935f7
  var keys = document.querySelectorAll('span');
  var previousInput = '';
  var currentInput = '';
  var operatorInput = '';
-<<<<<<< HEAD
  var operatorValue = false;
  var clearValue = false;
  var resultValue = false;
@@ -14,18 +9,12 @@
 
  var result = 0;
 
-=======
- var displayInput = document.querySelector('.screen');
-
- var result = 0;
->>>>>>> 92369294266b290c67fb83fcb707715cab4935f7
 // clears screen
  var clearScreen = function (){
      displayInput.innerHTML= "";
      previousInput = '';
      operatorInput = '';
      currentInput = '';
-<<<<<<< HEAD
      var operatorValue = false;
      var clearValue = false;
      var resultValue = false;
@@ -33,43 +22,6 @@
 
 //changes input to number
  var sanitizeHtmlInput = function (input) {
-=======
- }
-
-//display and clear keys
-for(i = 0; i < keys.length; i++) {
-   keys[i].onclick = function(keyOnClick) {
-       var keysValue = sanitizeHtmlInput(this.innerHTML);
-
-       if (typeof keysValue === 'number') {
-           displayInput.innerHTML += keysValue;
-       }
-
-       else if (keysValue === "C") {
-           clearScreen();
-       }
-
-       else {
-            currentInput = displayInput.innerHTML;
-            console.log(operatorInput);
-
-            if (previousInput && operatorInput) {
-                var result = performOperation(parseInt(previousInput, 10), parseInt(currentInput, 10), operatorInput);
-                    previousInput = result;
-                    displayInput.innerHTML = result;
-
-            } else {
-                previousInput = currentInput;
-                displayInput.innerHTML = "";
-            }
-
-            operatorInput = keysValue
-        }
-       }
-};
-
-var sanitizeHtmlInput = function (input) {
->>>>>>> 92369294266b290c67fb83fcb707715cab4935f7
     var newInput = parseInt(input, 10);
 
     if (isNaN(newInput)) {
@@ -79,10 +31,7 @@ var sanitizeHtmlInput = function (input) {
     }
 };
 
-<<<<<<< HEAD
 //performs a mathematical operation
-=======
->>>>>>> 92369294266b290c67fb83fcb707715cab4935f7
 var performOperation = function(firstNum, secondNum, operator){
     if (operator == '+') {
         result = firstNum + secondNum;
@@ -96,7 +45,6 @@ var performOperation = function(firstNum, secondNum, operator){
     return result;
 };
 
-<<<<<<< HEAD
 
 //display and clear keys
 for(i = 0; i < keys.length; i++) {
@@ -152,5 +100,3 @@ for(i = 0; i < keys.length; i++) {
    }
 };
 
-=======
->>>>>>> 92369294266b290c67fb83fcb707715cab4935f7
